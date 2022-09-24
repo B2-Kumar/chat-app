6d2ff705308789e7ca1d749987e359dd7c529dd4
Bittu <bittukumar1181@gmail.com>
2022-09-24 11:49:06 +0530

    changing login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
         <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
-          <button>Sign in</button>
+          <button>Sign-In</button>
           {err && <span>Something went wrong</span>}
         </form>
         <p>You don't have an account? <Link to="/register">Register</Link></p>
